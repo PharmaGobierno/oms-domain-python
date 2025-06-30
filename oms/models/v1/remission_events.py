3605f8e06fda272adb638490b0c72b11c99d2954
Ariel Villeda <ariel.villeda@cargamos.com>
2025-06-30 17:04:41 -0600

    model fix

--- oms/models/v1/remission_events.py
+++ oms/models/v1/remission_events.py
@@ -1,9 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Optional
 
 from oms.models.submodels.v1.remission_event_notes import RemissionEventNote
 from oms.models.submodels.v1.remission_evidences import RemissionEvidence
-
 from oms.models.v1.minified.users import UserMin
 
 from ._base import EventfulModel, uuid_by_params
@@ -22,7 +21,7 @@ class RemissionEventsModel(EventfulModel[RemissionEvents]):
     loaded_amount: Optional[int] = None
     delivered_amount: Optional[int] = None
     metadata: Optional[dict] = None
-    event_note: RemissionEventNote = RemissionEventNote()
+    event_note: RemissionEventNote = field(default_factory=RemissionEventNote)
 
     def __post_init__(self):
         super().__post_init__()
