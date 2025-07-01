c0ddaf74ae05b94a0f7317b23f5de189f5635e9f
Ariel Villeda <ariel.villeda@cargamos.com>
2025-06-30 23:51:54 -0600

    event_signature on migration published msg

--- oms/models/messages/v1/remission_events_migrations.py
+++ oms/models/messages/v1/remission_events_migrations.py
@@ -12,9 +12,14 @@ class RemissionEventMigrationsPubsubMessage(BasePubsubMessage):
     remission_id: str
     migration_log_id: str
     event: str
+    event_timestamp: int
+    event_signature: str = ""
     origin: ResmissionMigrationOrigins
     origin_id: str
     version: str = "1"
+
+    def __post_init__(self):
+        self.event_signature = self.event + str(self.event_timestamp) + self.origin_id
 
     @classmethod
     def topic(cls) -> str:
